ebf0f0108a2ecbbda33f5088b697eb95b21f2fd0
Randeep Bhatia <randeepbhatia50@gmail.com>
2015-02-05 17:29:33 -0800

    First checkin

---
+++ aws/aws-ios-header.h
@@ -0,0 +1,14 @@
+//
+//  aws-ios-header.h
+//  aws
+//
+//  Created by Bhatia, Randeep on 2/5/15.
+//  Copyright (c) 2015 maxis. All rights reserved.
+//
+
+#ifndef aws_aws_ios_header_h
+#define aws_aws_ios_header_h
+#import <AWSiOSSDKv2/AWSCore.h>
+#import "DynamoDB.h"
+
+#endif
